520acd87ba78c2699104f764911853a31e5213fe
Sathwik Reddy <sathwik1014@gmail.com>
2025-10-09 21:30:00 +0530

    fAdd About, Contact, Projects, and Resume components

---
+++ src/Components/Apps/About.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const About = () => (
+  <div className="h-screen bg-black text-white flex items-center justify-center">
+    <h1 className="text-4xl font-bold">About Me</h1>
+  </div>
+);
+
+export default About;
